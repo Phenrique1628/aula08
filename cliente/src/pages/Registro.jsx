d01492432b739734fa180608be7b56a0aa117ece
Phenrique1628 <henriquepedro02964@gmail.com>
2024-11-25 17:29:48 -0400

    Update Registro.jsx

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ListaProdutos from "../components/ListaProdutos";
 
 export default function Registrar() {
+  const [nome, setNome] = useState([]);
+  const [email, setEmail] = useState([]);
 
+  useEffect(() => {
+    const FormRegistro = async () => {
+      event.preventDefault();
+      try {
+        await fetch('http://localhost:3000/usuarios', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            nome: nome,
+            email: email
+          })
+        });
+      } catch{
+        alert("O código deu pau, press F")
+      }
+
+
+
+
+
+
+}
+  })
+  event.preventDefault();
 
   return (
-        
+
   );
 }
